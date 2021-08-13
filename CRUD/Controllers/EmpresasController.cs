0f5826e5bff14c3e11887b0291911a940d7b3317
Samuca <79856359+SamuelSilva2021@users.noreply.github.com>
2021-08-12 21:34:40 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD/Controllers/EmpresasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        private DBEmpresasEntities db = new DBEmpresasEntities();
+
+        // GET: Empresas
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Empresas.ToListAsync());
+        }
+
+        // GET: Empresas/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = await db.Empresas.FindAsync(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // GET: Empresas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empresas/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Nome,Site,QuantidadeFuncionarios")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empresas.Add(empresa);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(empresa);
+        }
+
+        // GET: Empresas/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = await db.Empresas.FindAsync(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: Empresas/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Nome,Site,QuantidadeFuncionarios")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empresa).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(empresa);
+        }
+
+        // GET: Empresas/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = await db.Empresas.FindAsync(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: Empresas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Empresa empresa = await db.Empresas.FindAsync(id);
+            db.Empresas.Remove(empresa);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
